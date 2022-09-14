08d933675312a0fc6173eda621331bfd8d27acaf
Keyvan Mosharraf <keyvan@keyvanm.com>
2022-09-14 13:02:31 -0400

    test stubs

--- tests/test_querysets.py
+++ tests/test_querysets.py
@@ -4,3 +4,27 @@ def test_RankedQuerySetMixin():
 
 def test_RankedQuerySetMixin_asc():
     pass
+
+
+def test_PageableQuerySet_paginate_minmax():
+    pass
+
+
+def test_PageableQuerySet_paginate_pks():
+    pass
+
+
+def test_PageableQuerySet_paginate_pks_not_simple():
+    pass
+
+
+def test_PageableQuerySet_paginate_pks_mutating():
+    pass
+
+
+def test_PageableQuerySet_paginate():
+    pass
+
+
+def test_PageableQuerySet_paginated_update():
+    pass
