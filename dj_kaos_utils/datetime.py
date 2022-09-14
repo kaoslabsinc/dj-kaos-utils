648d648d38299ef5da44a2c44de6990aa119a7d4
Keyvan Mosharraf <keyvan@keyvanm.com>
2022-09-14 13:19:21 -0400

    parse_and_make_tz_aware and tests

---
+++ dj_kaos_utils/datetime.py
@@ -0,0 +1,17 @@
+from dateutil import parser
+from django.utils.timezone import is_aware, make_aware
+
+
+def parse_and_make_tz_aware(s: str | None):
+    """
+    Parse the string s into a datetime object and make it timezone aware if it is not so already.
+    :param s: String to be parsed into a timezone
+    :return: Timezone aware datetime instance
+    """
+    if s is None:
+        return
+
+    dt = parser.parse(s)
+    if not is_aware(dt):
+        dt = make_aware(dt)
+    return dt
