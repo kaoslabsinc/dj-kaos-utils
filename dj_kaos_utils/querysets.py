175b9963e600df96a1eb1b5b86331aadcf2a8a23
Keyvan Mosharraf <keyvan@keyvanm.com>
2022-09-14 13:27:16 -0400

    Typing variables

--- dj_kaos_utils/querysets.py
+++ dj_kaos_utils/querysets.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Callable, TypeVar
 
 from django.core.paginator import Paginator
 from django.db import models, transaction
@@ -7,6 +8,8 @@ from django.db.models.functions import Rank
 
 logger = logging.getLogger()
 
+QS = TypeVar('QS', bound=models.QuerySet)
+
 
 class RankedQuerySetMixin(models.QuerySet):
     """
@@ -14,7 +17,7 @@ class RankedQuerySetMixin(models.QuerySet):
     row based on a field
     """
 
-    def annotate_rank(self, field, asc=False):
+    def annotate_rank(self: QS, field, asc=False) -> QS:
         """
         Annotate the rank of each row based on the values in field.
         :param field: Rank entries based on values in field
@@ -26,7 +29,7 @@ class RankedQuerySetMixin(models.QuerySet):
 
 
 class PageableQuerySet(models.QuerySet):
-    def paginate_minmax(self, limit):
+    def paginate_minmax(self: QS, limit: int) -> QS:
         d = self.values('id').aggregate(min=Min('id'), max=Max('id'))
         min_id, max_id = d['min'], d['max']
         if min_id is None:
@@ -34,7 +37,7 @@ class PageableQuerySet(models.QuerySet):
         for i in range(min_id, max_id + 1, limit):
             yield self.filter(id__gte=i, id__lt=i + limit)
 
-    def paginate_pks(self, limit, simple=True, mutating=False):
+    def paginate_pks(self: QS, limit: int, simple: bool = True, mutating: bool = False) -> QS:
         qs = self.model.objects.all() if simple else self
         pk_values = self.values_list('pk', flat=True)
         if mutating:
@@ -43,13 +46,20 @@ class PageableQuerySet(models.QuerySet):
         for page in Paginator(pk_values, limit):
             yield qs.filter(pk__in=page.object_list)
 
-    def paginate_pks_mutating(self, limit, simple=True):
+    def paginate_pks_mutating(self: QS, limit, simple: bool = True) -> QS:
         return self.paginate_pks(limit, simple=simple, mutating=True)
 
-    def paginate(self, limit):
+    def paginate(self: QS, limit: int) -> QS:
         return self.paginate_minmax(limit)
 
-    def paginated_update(self, limit, page_op):
+    def paginated_update(self, limit: int, page_op: Callable[[models.QuerySet], int]) -> int:
+        """
+        Run operation page_op on the queryset page by page
+        :param limit: Page size
+        :param page_op: Operation to run on each page. It should at the end update the database and return the number of
+        rows updated
+        :return: the total number of rows updated.
+        """
         opts = self.model._meta
         verbose_name_plural = opts.verbose_name_plural
         total_count = self.count()
