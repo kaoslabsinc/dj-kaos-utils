0640f5954535699da742dd9f2910ff5b001ea252
Keyvan Mosharraf <keyvan@keyvanm.com>
2022-09-19 16:46:33 -0400

    Cover clean too in test

--- tests/models/test_mixins.py
+++ tests/models/test_mixins.py
@@ -1,8 +1,15 @@
 from simple.models import Category
 
 
-def test_HasAutoFields(db):
-    category = Category(name="Name")
+def test_HasAutoFields_save(db):
+    category = Category(name="Cat")
     assert not category.slug
     category.save()
+    assert category.slug
+
+
+def test_HasAutoFields_clean(db):
+    category = Category(name="Cat")
+    assert not category.slug
+    category.clean()
     assert category.slug
