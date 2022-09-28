fe79403355328911c519609aeba3cedff7a72519
Keyvan Mosharraf <keyvan@keyvanm.com>
2022-09-27 17:37:58 -0700

    HasInitials

--- dj_kaos_utils/models/mixins.py
+++ dj_kaos_utils/models/mixins.py
@@ -1,3 +1,6 @@
+from py_kaos_utils.string import create_initials
+
+
 class HasAutoFields:
     """
     Mixin for models that have fields that can be automatically set, for example from the value of other fields.
@@ -15,6 +18,22 @@ class HasAutoFields:
         super(HasAutoFields, self).save(*args, **kwargs)
 
 
+class HasInitials:
+    """
+    Add property `initials` to its inheritor classes. Take the value form the field defined by `take_initials_from` and
+    return the initial letters of each word in it, capitalized.
+    Example: John Smith => JS
+    """
+    take_initials_from = None
+
+    @property
+    def initials(self):
+        if self.take_initials_from is None:
+            raise AttributeError(f"take_initials_from not defined on {self.__class__}")
+        return create_initials(getattr(self, self.take_initials_from))
+
+
 __all__ = [
     'HasAutoFields',
+    'HasInitials',
 ]
