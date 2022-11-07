379ac617724c1039d9be997a8b13dfc2b2105539
Ben Gerdemann <github@ibeni.net>
2022-11-07 14:58:05 +0000

    Add test_serializer_create_update_do_create test_serializer_create_update_do_update

--- tests/rest/test_serializers.py
+++ tests/rest/test_serializers.py
@@ -1,11 +1,9 @@
-from django.utils.safestring import mark_safe
-
-from dj_kaos_utils.admin.utils import pp_json, _render_attrs, render_element, render_img, render_anchor
 from dj_kaos_utils.rest.serializers import make_nested_writable
 
 from simple.models import Product
 from rest_framework.serializers import ModelSerializer
 import pytest
+
 
 class ProductModelSerializer(ModelSerializer):
 
@@ -16,30 +14,31 @@ class ProductModelSerializer(ModelSerializer):
             'price',
             'code_id',
         )
-        lookup_field ='id'
+        lookup_field = 'id'
+
 
 @pytest.fixture
 def created_product():
     return Product.objects.create(
-        name = "Created Product",
-        price = "1.00",
-        code_id = 'code_id_1',
+        name="Product 1",
+        price="1.00",
+        code_id='code_id_1',
     )
 
-def test_serializer_create(db):
-    nested_writable = make_nested_writable(ProductModelSerializer, can_create=True)
+
+def _test_create(nested_writable):
     data = {
-        'name': "Product 1",
+        'name': "Created Product",
         'price': '1.00',
-        'code_id': 'id01',
+        'code_id': 'code_id_1',
     }
     serializer = nested_writable(data=data)
     assert serializer.is_valid()
-    created_obj = serializer.validated_data
-    Product.objects.get(id=created_obj.id)
+    obj = serializer.validated_data
+    Product.objects.get(id=obj.id)
 
-def test_serializer_update(db, created_product):
-    nested_writable = make_nested_writable(ProductModelSerializer, can_update=True)
+
+def _test_update(created_product, nested_writable):
     data = {
         'id': created_product.id,
         'name': "New Name",
@@ -50,8 +49,8 @@ def test_serializer_update(db, created_product):
     assert updated_obj.id == created_product.id
     assert updated_obj.name == "New Name"
 
-def test_serializer_get(db, created_product):
-    nested_writable = make_nested_writable(ProductModelSerializer, can_get=True)
+
+def _test_get(created_product, nested_writable):
     data = {
         'id': created_product.id,
     }
@@ -59,3 +58,35 @@ def test_serializer_get(db, created_product):
     assert serializer.is_valid()
     obj = serializer.validated_data
     assert obj.id == created_product.id
+
+
+def test_serializer_create(db):
+    nested_writable = make_nested_writable(ProductModelSerializer,
+                                           can_create=True)
+    _test_create(nested_writable)
+
+
+def test_serializer_update(db, created_product):
+    nested_writable = make_nested_writable(ProductModelSerializer,
+                                           can_update=True)
+    _test_update(created_product, nested_writable)
+
+
+def test_serializer_get(db, created_product):
+    nested_writable = make_nested_writable(ProductModelSerializer,
+                                           can_get=True)
+    _test_get(created_product, nested_writable)
+
+
+def test_serializer_create_update_do_create(db):
+    nested_writable = make_nested_writable(ProductModelSerializer,
+                                           can_create=True,
+                                           can_update=True)
+    _test_create(nested_writable)
+
+
+def test_serializer_create_update_do_update(db, created_product):
+    nested_writable = make_nested_writable(ProductModelSerializer,
+                                           can_create=True,
+                                           can_update=True)
+    _test_update(created_product, nested_writable)
