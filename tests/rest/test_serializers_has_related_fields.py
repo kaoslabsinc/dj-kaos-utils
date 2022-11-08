3a57dcda54b47d04d5a5eea8bdb2cb8071a7be69
Ben Gerdemann <github@ibeni.net>
2022-11-08 11:20:56 +0000

    Add test_serializer_update_replace_obj

--- tests/rest/test_serializers_has_related_fields.py
+++ tests/rest/test_serializers_has_related_fields.py
@@ -30,12 +30,13 @@ class CategorySerializer(HasRelatedFieldsModelSerializer,
 
 
 @pytest.fixture
-def create_category():
-    return Product.objects.create(
-        name="Product 1",
-        price="1.00",
-        code_id='code_id_1',
-    )
+def category_1_data():
+    return dict(name="Category 1")
+
+
+@pytest.fixture
+def category_1(category_1_data):
+    return Category.objects.create(**category_1_data)
 
 
 @pytest.fixture
@@ -46,6 +47,29 @@ def create_product(create_category):
         code_id='code_id_1',
         category=create_category,
     )
+
+
+@pytest.fixture
+def product_1_data():
+    return dict(
+        name="Product 1",
+        price='1.00',
+        code_id='code_id_1',
+    )
+
+
+@pytest.fixture
+def product_2_data():
+    return dict(
+        name="Product 2",
+        price='2.00',
+        code_id='code_id_2',
+    )
+
+
+@pytest.fixture
+def product_1(product_1_data):
+    return Product.objects.create(**product_1_data)
 
 
 def test_serializer_create(db):
@@ -83,3 +107,19 @@ def test_serializer_create_multiple(db):
     assert created_category.products.count() == 2
     created_product_1 = created_category.products.get(name=product_1['name'])
     created_product_2 = created_category.products.get(name=product_2['name'])
+
+
+def test_serializer_update_replace_obj(db, category_1_data, category_1,
+                                       product_1, product_2_data):
+    category_1.products.set([product_1])
+    data = dict(
+        products=[product_2_data],
+        name=category_1.name,
+        id=category_1.id,
+    )
+    serializer = CategorySerializer(data=data)
+    assert serializer.is_valid()
+    created_category = serializer.save()
+    assert created_category.products.count() == 1
+    created_product_1 = created_category.products.get(
+        name=product_2_data['name'])
