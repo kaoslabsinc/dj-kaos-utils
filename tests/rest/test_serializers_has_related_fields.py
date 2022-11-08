a72c28bc028cea1f29d17f1b83523febbd451ba0
Ben Gerdemann <github@ibeni.net>
2022-11-08 11:09:41 +0000

    Add test_serializer_create_multiple

--- tests/rest/test_serializers_has_related_fields.py
+++ tests/rest/test_serializers_has_related_fields.py
@@ -61,3 +61,25 @@ def test_serializer_create(db):
     assert serializer.is_valid()
     created_category = serializer.save()
     assert created_category.products.count() == 1
+
+
+def test_serializer_create_multiple(db):
+    product_1 = dict(
+        name="Product 1",
+        price='1.00',
+        code_id='code_id_1',
+    )
+    product_2 = dict(
+        name="Product 2",
+        price='2.00',
+        code_id='code_id_2',
+    )
+    products = [product_1, product_2]
+    category_data = dict(name="Category 1", products=products)
+
+    serializer = CategorySerializer(data=category_data)
+    assert serializer.is_valid()
+    created_category = serializer.save()
+    assert created_category.products.count() == 2
+    created_product_1 = created_category.products.get(name=product_1['name'])
+    created_product_2 = created_category.products.get(name=product_2['name'])
