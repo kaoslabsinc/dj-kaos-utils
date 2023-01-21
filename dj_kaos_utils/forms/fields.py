2d5edb5250f4f79eed292ded1865df7013ce3f81
Keyvan Mosharraf <keyvanm@users.noreply.github.com>
2023-01-21 22:14:06 +0000

    docstring

--- dj_kaos_utils/forms/fields.py
+++ dj_kaos_utils/forms/fields.py
@@ -3,13 +3,34 @@ from django.utils.html import format_html_join, format_html
 
 
 class ListTextWidget(forms.TextInput):
+    """
+    A Django form widget that renders a text input with a datalist populated with options from a given list.
+
+    Example:
+    >>> class MyForm(forms.Form):
+    >>>     my_field = forms.CharField(widget=ListTextWidget(datalist=['option1', 'option2', 'option3']))
+    """
+
     def __init__(self, datalist, name, *args, **kwargs):
+        """
+        Initialize the widget with the list of options and the name of the datalist.
+
+        :param datalist: list of options for the datalist
+        :type datalist: list
+        :param name: name of the datalist
+        :type name: str
+        """
+
         super(ListTextWidget, self).__init__(*args, **kwargs)
         self._name = name
         self._list = datalist
         self.attrs.update({'list': f'list__{name}'})
 
     def render(self, name, value, attrs=None, renderer=None):
+        """
+        Render the text input and datalist.
+        """
+
         datalist_html = format_html('<datalist id="list__{name}">{options}</datalist>',
                                     name=self._name,
                                     options=format_html_join('', '<option value="{}">', self._list))
