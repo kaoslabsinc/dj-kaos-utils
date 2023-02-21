090e02c9ab8596382ad63e1280cf1d6a07708ad6
Keyvan Mosharraf <keyvan@keyvanm.com>
2023-02-20 18:00:56 -0800

    Use DTWrapper from py-kaos-utils==0.2.0-sandbox1

--- dj_kaos_utils/datetime.py
+++ dj_kaos_utils/datetime.py
@@ -1,12 +1,18 @@
 from __future__ import annotations
 
+import datetime as dt
+from typing import Optional
+from zoneinfo import ZoneInfo
+
 from django.conf import settings
 from py_kaos_utils.datetime import DatetimeWrapper as PyDTWrapper
 
 
 class DatetimeWrapper(PyDTWrapper):
-    def __init__(self, raw_datetime, timezone_str=settings.TIME_ZONE):
-        super().__init__(raw_datetime, timezone_str)
+    def __init__(self,
+                 raw_dt: str | dt.datetime,
+                 raw_tz: Optional[str | ZoneInfo] = settings.TIME_ZONE):
+        super().__init__(raw_dt, raw_tz)
 
 
 DT = DatetimeWrapper
--- tests/test_datetime.py
+++ tests/test_datetime.py
@@ -1,25 +1,12 @@
 import datetime
-import zoneinfo
+from zoneinfo import ZoneInfo
 
-from dj_kaos_utils.datetime import parse_and_make_tz_aware
+from django.conf import settings
 
-
-def test_parse_and_make_tz_aware_None():
-    assert parse_and_make_tz_aware(None) is None
+from dj_kaos_utils.datetime import DT
 
 
-def test_parse_and_make_tz_aware_no_tz_str():
-    intended_dt = datetime.datetime(2022, 8, 5, 12, 34, tzinfo=zoneinfo.ZoneInfo(key='UTC'))
-    assert parse_and_make_tz_aware("Aug 5th 2022 12:34pm") == intended_dt
-
-
-def test_parse_and_make_tz_aware_no_tz_str_non_utc_tz(settings):
-    settings.TIME_ZONE = 'America/Vancouver'
-
-    intended_dt = datetime.datetime(2022, 8, 5, 12, 34, tzinfo=zoneinfo.ZoneInfo(key='America/Vancouver'))
-    assert parse_and_make_tz_aware("Aug 5th 2022 12:34pm") == intended_dt
-
-
-def test_parse_and_make_tz_aware():
-    intended_dt = datetime.datetime(2022, 8, 5, 20, 34, tzinfo=zoneinfo.ZoneInfo(key='UTC'))
-    assert parse_and_make_tz_aware("Aug 5th 2022 12:34pm -08:00").timestamp() == intended_dt.timestamp()
+class TestDatetimeWrapper:
+    def test_timezone(self):
+        wrapped_dt = DT(datetime.datetime(2022, 1, 1, 12, 30, 0))
+        assert wrapped_dt.tz == ZoneInfo(settings.TIME_ZONE)
